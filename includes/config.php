d13f6efb0116bb2c9db48b4758ca4753e97bc6fd
Katrina H <katrina.hitchcock@gmail.com>
2020-07-23 00:14:45 +0200

    add heroku db config

--- includes/config.php
+++ includes/config.php
@@ -4,7 +4,7 @@ session_start();
 date_default_timezone_set("Europe/London");
 
 try {
-    $con = new PDO('mysql:dbname=premiere;host=localhost', 'root', '');
+    $con = new PDO('mysql:dbname=heroku_171e059e0ec155d;host=us-cdbr-east-02.cleardb.com', 'b20d0c989d1161', '0b374556');
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 } catch (PDOException $e) {
     exit('Connection failed:' . $e->getMessage());
