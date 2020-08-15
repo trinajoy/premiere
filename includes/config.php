a76eca646b03e49278b1f18a28710033fdebdd0d
Katrina H <katrina.hitchcock@gmail.com>
2020-08-15 11:43:02 +0200

    update db host

--- includes/config.php
+++ includes/config.php
@@ -4,11 +4,11 @@ session_start();
 date_default_timezone_set("Europe/London");
 
 try {
-
-    $con = new PDO('mysql:dbname=heroku_ae7d9830407b3ff;host=eu-cdbr-west-03.cleardb.net', 'b64dd9304457cf', 'eb2c3cde');
-    // $con = new PDO('mysql:dbname=premiere;host=localhost', 'root', '');
-
+    $con = new PDO("mysql:dbname=guest;host=mypremiereserver.mysql.database.azure.com", "myadmin@mypremiereserver", "Password123");
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 } catch (PDOException $e) {
     exit('Connection failed:' . $e->getMessage());
 }
+
+
+//$con = new PDO('mysql:dbname=premiere;host=localhost', 'root', '');
